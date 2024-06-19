d6c5194941c7629dfc75b972ac7408698e85b46f
User Name <safia@safias-MacBook-Air.local>
2024-06-19 15:45:04 -0400

    added sound files

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+let width = window.innerWidth;
+let height = window.innerHeight;
+
 function setup() {
-  // put setup code here
+  canvas = createCanvas(weight, height);
 }
 
 function draw() {
-  // put drawing code here
+  background(247, 178, 183);
 }
+class Button {
+  constructor(x, y, w, h, color, accent, song) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+    this.accent = accent;
+    this.song = song;
+  }
+}
+
+show() {
+  noStroke();
+
+  fill(this.color); 
+  rect(this.x, this.y, this.w, this.h);
+
+  fill(this.accent); 
+  ellipse(this.x, this.y, this.w, this.h);
+
+  fill(this.color); 
+  arc(this.x, this.y, this.w, this.h, TWO_PI, PI);
+};
